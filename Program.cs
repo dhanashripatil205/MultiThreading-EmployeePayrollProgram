19468daa6cf9bdfe58f5185c33ff2234d806de98
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-02-07 22:15:44 +0530

    Greeting Message!!!

--- Program.cs
+++ Program.cs
@@ -4,26 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            EmployeeRepo employeeRepo = new EmployeeRepo();
-            EmployeeModel employeeModel = new EmployeeModel();
-
-            employeeModel.EmployeeName = "Prithvi";
-            employeeModel.PhoneNumber = "9898989889";
-            employeeModel.Address = "13 street";
-            employeeModel.Department = "Hr";
-            employeeModel.Gender = 'M';
-            employeeModel.BasicPay = 20000;
-            employeeModel.Deductions = 200;
-            employeeModel.TaxablePay = 2500;
-            employeeModel.Tax = 1000;
-            employeeModel.NetPay = 200;
-            employeeModel.City = "Banglore";
-            employeeModel.Country = "India";
-
-            //employeeRepo.AddEmployee(employeeModel);
-            //employeeRepo.GetAllEmployee();
-            EmployeeRepo.GetAllEmployee();
-
+            Console.WriteLine("*****Welcome to Employee Payroll Problem Using MultiThreading.*****");
         }
     }
 }
